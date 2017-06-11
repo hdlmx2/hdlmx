d40f3ecc8b55517c1db8db88617777ef31b4978b
handong <hdlmx@outlook.com>
2017-06-11 16:51:31 +0800

    最终解决了bug。在controller中返回视图时，出现只能路由至RequestMapping中值，相同名称的视图。原因是controller中的ModelAndView引用的不是servlet包下的。而是另一个包下的，这里要研究一下。

--- src/cn/hdlmx/controller/UserController.java
+++ src/cn/hdlmx/controller/UserController.java
@@ -1,7 +1,29 @@
 package cn.hdlmx.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserController {
+    @RequestMapping(value = "/signin", method = RequestMethod.GET)
+    public ModelAndView signin(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println(request.getContextPath());
+        ModelAndView modelAndView = new ModelAndView("success");
+        modelAndView.setViewName("success");
+        modelAndView.addObject("欢迎", "韩东");
+        return modelAndView;
+
+    }
+
+    @RequestMapping("/sign")
+    public String sign(Model model, HttpServletRequest request, HttpServletResponse response) {
+        model.addAttribute("user", "hdlmx");
+        return "success";
+    }
 }
--- web/success.jsp
+++ web/success.jsp
@@ -1,16 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
   User: handong
-  Date: 2017/6/10
-  Time: 12:25
+  Date: 2017/6/11
+  Time: 11:18
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
+<head>
     <title>hdlmx</title>
-  </head>
-  <body>
-  <h1>hdlmx</h1>
-  </body>
+</head>
+<body>
+<h1>${欢迎}</h1>
+</body>
 </html>
